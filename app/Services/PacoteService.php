30ea4189d5403f0e85cdbea1c83a88d4392f466e
Robson Venceslau <ravences@gmail.com>
2018-08-22 22:58:21 -0300

    Tratamento de resposta da api com HTTP STATUS.

--- app/Services/PacoteService.php
+++ app/Services/PacoteService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Illuminate\Http\Request;
 use App\Repositories\PacoteRepositoryInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class PacoteService
 {
@@ -16,32 +17,62 @@ class PacoteService
 
     public function buscarTodosPacotes()
     {
-        $pacotes = $this->pacoteRepository->buscarTodosPacotes();
+        try{
+            $pacotes = $this->pacoteRepository->buscarTodosPacotes();
 
-        if(count($pacotes) > 0){
-            return $pacotes;
-        }else{
-            return array();
+            if(count($pacotes) > 0){
+                return response()->json($pacotes, Response::HTTP_OK);
+            }else{
+                return response()->json([], Response::HTTP_OK);
+            }
+        }catch(QueryException $e){
+            return response()->json(['erro'=> 'Erro de conexão com o banco'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
     public function buscarPacote($id)
     {
-        return $this->pacoteRepository->buscarPacote($id);
+        try{
+            $pacote = $this->pacoteRepository->buscarPacote($id);
+
+            if(count($pacote) > 0){
+                return response()->json($pacote, Response::HTTP_OK);
+            }else{
+                return response()->json(null, Response::HTTP_NOT_FOUND);
+            }
+
+        }catch(QueryException $e){
+            return response()->json(['erro'=> 'Erro de conexão com o banco'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function criarPacote(Request $request)
     {
-        return $this->pacoteRepository->criarPacote($request);
+        try{
+            $pacote = $this->pacoteRepository->criarPacote($request);
+            return response()->json($pacote, Response::HTTP_CREATED);
+        }catch(QueryException $e){
+            return response()->json(['Erro'=> 'Erro de conexão com o banco'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function editarPacote(int $id, Request $request)
     {
-        return $this->pacoteRepository->editarPacote($id, $request);
+        try{
+            $pacote = $this->pacoteRepository->editarPacote($id, $request);
+            return response()->json($pacote, Response::HTTP_OK);
+        }catch(QueryException $e){
+            return response()->json(['Erro'=> 'Erro de conexão com o banco'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function excluirPacote(int $id)
     {
-        return $this->pacoteRepository->excluirPacote($id);
+        try{
+            $pacote = $this->pacoteRepository->excluirPacote($id);
+            return response()->json(null, Response::HTTP_OK);
+        }catch(QueryException $e){
+            return response()->json(['Erro'=> 'Erro de conexão com o banco'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
